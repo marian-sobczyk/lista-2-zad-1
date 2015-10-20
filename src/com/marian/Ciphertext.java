3edc2b83f3541287722815db370e675a5513f0e8
Marian Sobczyk <marian.sobczyk@gmail.com>
2015-10-20 21:40:08 +0200

    Added Ciphertext

---
+++ src/com/marian/Ciphertext.java
@@ -0,0 +1,23 @@
+package com.marian;
+
+import java.util.ArrayList;
+
+/**
+ * Created by marian on 20.10.15.
+ */
+public class Ciphertext {
+    private final ArrayList<Character> characters;
+
+    public Ciphertext(String text) {
+        String[] numbers = text.split(" ");
+        ArrayList<Character> list = new ArrayList<Character>();
+        for (int i = 0; i < numbers.length; i++) {
+            String number = numbers[i];
+            if (number.length() >= 8) {
+                char character = (char) Integer.parseInt(number);
+                list.add(character);
+            }
+        }
+        this.characters = list;
+    }
+}
--- src/com/marian/Main.java
+++ src/com/marian/Main.java
@@ -1,14 +1,21 @@
 package com.marian;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
+    private static ArrayList<Ciphertext> ciphertexts;
+
     public static void main(String[] args) {
+
+        ciphertexts = new ArrayList<Ciphertext>();
 
         try {
             for (int i = 0; i <= 20; i++) {
                 String text = CipherReader.getCipherFromFile(i);
+                Ciphertext ciphertext = new Ciphertext(text);
+                ciphertexts.add(ciphertext);
                 System.out.println(text);
             }
         } catch (IOException e) {
